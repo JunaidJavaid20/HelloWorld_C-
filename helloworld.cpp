b2e16b0eaa1b1e1846841db1c8394ceedb617c66
JunaidJavaid20 <junaidjavaid203.1@gmail.com>
2024-02-29 16:07:10 +0500

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello World";
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int  main(){
+    cout <<"C++ Hello world assignment done:";
+    return 0;
+}
